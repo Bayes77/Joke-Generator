bbacdfcf41131317501300bc4ac553ba105ae8ad
Bayez77 <mrbayez88@gmail.com>
2024-08-17 13:59:50 -0500

    setup

--- public/main.js
+++ public/main.js
@@ -2,24 +2,27 @@
 // import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
+import getRequest from '../api/Promises';
 
-const init = () => {
+const htmlStructure = () => {
   document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
+  <h1>Joke Generator</h1>
+  <button type="button" id="get-joke" class="btn-primary">Get Joke</button>
   `;
-  console.warn('YOU ARE UP AND RUNNING!');
-
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // ViewDirectorBasedOnUserAuthStatus();
 };
 
-init();
+const events = () => {
+  document.querySelector('#get-joke').addEventListener('click', () => {
+    getRequest().then((response) => {
+      console.warn(response.setup);
+      console.warn(response.delivery);
+    });
+    console.warn('Hey There !!');
+  });
+};
+const startApp = () => {
+  htmlStructure();
+  events();
+};
+
+startApp();
