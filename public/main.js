67f99640672c10b6d37285c51adcfcb881785776
Bayez77 <mrbayez88@gmail.com>
2024-08-18 20:21:36 -0500

    startup

--- public/main.js
+++ public/main.js
@@ -7,17 +7,33 @@ import getRequest from '../api/Promises';
 const htmlStructure = () => {
   document.querySelector('#app').innerHTML = `
   <h1>Joke Generator</h1>
+  <div id="getJoke">
+  <span id="setup"></span>
+  <span id="punchline"  class= "hidden"></span>
+  </div>
   <button type="button" id="get-joke" class="btn-primary">Get Joke</button>
+   <button type="button" id="get-punchline" class="btn-primary hidden" >Get punchline</button>
   `;
 };
 
 const events = () => {
   document.querySelector('#get-joke').addEventListener('click', () => {
     getRequest().then((response) => {
+      document.querySelector('#setup').innerHTML = `<span>${response.setup}</span>`;
+      document.querySelector('#punchline').innerHTML = `<span  >${response.delivery}</span>`;
+      document.querySelector('#get-punchline').classList.remove('hidden');
+      document.querySelector('#get-joke').classList.add('hidden');
+      document.querySelector('#punchline').classList.add('hidden');
+      document.querySelector('#get-punchline').innerHTML = 'get-punchline';
       console.warn(response.setup);
       console.warn(response.delivery);
     });
     console.warn('Hey There !!');
+  });
+  document.querySelector('#get-punchline').addEventListener('click', () => {
+    document.querySelector('#punchline').classList.remove('hidden');
+    document.querySelector('#get-punchline').classList.add('hidden');
+    document.querySelector('#get-joke').classList.remove('hidden');
   });
 };
 const startApp = () => {
